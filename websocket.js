dce9a2d4c8df7aa1517765a72b1f34a2062a8702
Gianluca <gian.hd@gmail.com>
2022-07-07 10:01:44 -0300

    Ajustes

--- websocket.js
+++ websocket.js
@@ -1,24 +1,64 @@
 const WebSocket = require('ws');
+const uuid = require('uuid');
+
+const map = new Map();
 
 const server = new WebSocket.Server({
-    port: 3000
+    port: 3000,
+    path: '/websockets',
 });
+
+function onMessage(ws, data) {
+
+    console.log(`onMessage: ${data}`);
+
+    let dataJson = JSON.parse(data);
+
+    let wsTo = null;
+
+    for (const [key, value] of map.entries()) {
+        console.log('key', key);
+        if (key == dataJson.userTo) {
+            wsTo = value;
+            break;
+        }
+    }
+
+    if (wsTo == null) {
+        ws.send("User invalid!");
+    }
+
+    server.clients.forEach(function each(client) {
+        // if (client !== server && client.readyState === WebSocket.OPEN) {
+        //     client.send("Thanks!");
+        // }
+        if (client == wsTo) {
+            client.send(dataJson.message);
+        }
+    });
+
+}
 
 function onError(ws, err) {
     console.error(`onError: ${err.message}`);
 }
 
-function onMessage(ws, data) {
-    console.log(`onMessage: ${data}`);
-    let dataJson = JSON.parse(data);
-    ws.send(`result: ${dataJson.value1 + dataJson.value2}`);
-}
+server.on('connection', function (ws, request) {
 
-function onConnection(ws, req) {
+    const userId = uuid.v4();
+    map.set(userId, ws);
+
     ws.on('message', data => onMessage(ws, data));
-    ws.on('error', error => onError(ws, error));
-    console.log(`onConnection`);
-}
 
-server.on('connection', onConnection);
-console.log(`App Web Socket Server is running!`);
+    ws.on('error', error => onError(ws, error));
+
+    ws.on('close', function () {
+        map.delete(userId);
+        console.log(`onClose: WebSocket ${userId} has closed`);
+    });
+
+    console.log(`onConnection: WebSocket ${userId} has connected`);
+
+});
+
+console.log(`App WebSocket Server is running!`);
